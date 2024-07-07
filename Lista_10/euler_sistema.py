72786026cc5a81f36da224c326083cf992446863
Eros <erosstark@gmail.com>
2024-07-07 17:32:02 -0300

    fixed variable on EDOs func

--- Lista_10/euler_sistema.py
+++ Lista_10/euler_sistema.py
@@ -6,8 +6,8 @@ def EDOs(f,S0,t0,tf,n):
     t = np.zeros(n+1)
     S = np.zeros((n+1,len(S0))) # S[i,k] solução equação k no tempo t[i]
     S[0] = S0
-    t[0] = ti
-    h = float(tf-ti)/n
+    t[0] = t0
+    h = float(tf-t0)/n
     for i in range(n):
         t[i+1] = t[i] + h
         S[i+1] = S[i] + h * f(S[i], t[i])
