fd115dc227e43c8af69e1fbf01f45378cd680b21
Eros <erosstark@gmail.com>
2024-07-08 13:01:16 -0300

    add *args

--- Lista_10/euler_syst.py
+++ Lista_10/euler_syst.py
@@ -1,6 +1,20 @@
 import numpy as np
 
-def Euler(f,S0,t0,tf,n):
+def Euler(f,S0,t0,tf,n, *args):
+    """
+    Calcula a solução para um sistema de EDOs de primeira ordem
+    pelo método de Euler	
+
+    Args:
+        f (func): função do sistema de EDOs
+        S0 (int, float or list): Condições iniciais
+        t0 (_type_): tempo inicial
+        tf (_type_): tempo final
+        n (_type_): número de iterações
+
+    Returns:
+        tuple: _solução do sistema de EDOs, tempo 
+    """
     t = np.zeros(n+1)
     if isinstance(S0, (float, int)):  #True if S0 is a list of floats or ints
         S = np.zeros(n+1)  # S[k] is the solution at time t[k]
@@ -9,7 +23,14 @@ def Euler(f,S0,t0,tf,n):
     S[0] = S0
     t[0] = t0
     h = float(tf-t0)/n
-    for i in range(n):
-        t[i+1] = t[i] + h
-        S[i+1] = S[i] + h * f(S[i], t[i])
+    
+    if args:
+        for i in range(n):
+            t[i+1] = t[i] + h
+            S[i+1] = S[i] + h * f(S[i], t[i], args)
+    else:
+        for i in range(n):
+            t[i+1] = t[i] + h
+            S[i+1] = S[i] + h * f(S[i], t[i])
+            
     return  S, t
