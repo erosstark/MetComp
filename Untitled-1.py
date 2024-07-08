b29275b15d117736b33d5eacec7b86bc09402254
Eros <erosstark@gmail.com>
2024-07-08 14:18:48 -0300

    testing zip

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,13 @@
-#%% 
-import numpy as np
-a  = np.linspace(0,1,10)
-
-len(a)
 #%%
+
+a : dict= {'a': 1, 'b': 2, 'c': 3}
+
+b = a["a"]
+
+# %%
+b
+# %%
+
+for i,j in zip([0,0,0], range(0,3)):
+    print(i,j)
+# %%
