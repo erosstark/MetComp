a68aa18547322e9efb1482dfab149f04980196f0
Eros <erosstark@gmail.com>
2024-07-11 19:20:52 -0300

    teste

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,13 +1,35 @@
-#%%
+from manim import *
 
-a : dict= {'a': 1, 'b': 2, 'c': 3}
+class SphericalPendulum(Scene):
+    def construct(self):
+        # Dados da simulação
+        x_data = [...]  # suas coordenadas x
+        y_data = [...]  # suas coordenadas y
+        z_data = [...]  # suas coordenadas z
+        t_data = [...]  # seus tempos
 
-b = a["a"]
+        # Verificar se os dados estão alinhados
+        assert len(x_data) == len(y_data) == len(z_data) == len(t_data), "Os dados devem ter o mesmo comprimento"
 
-# %%
-b
-# %%
+        pendulum_origin = ORIGIN
+        pendulum_mass = Dot(pendulum_origin)
+        rod = Line(pendulum_origin, pendulum_mass.get_center())
+        self.add(rod, pendulum_mass)
 
-for i,j in zip([0,0,0], range(0,3)):
-    print(i,j)
-# %%
+        def update_pendulum(mob, alpha):
+            # Encontrar o índice correspondente ao valor de alpha
+            idx = min(int(alpha * len(t_data)), len(t_data) - 1)
+            x = x_data[idx]
+            y = y_data[idx]
+            z = z_data[idx]
+
+            # Atualizar a posição do pêndulo
+            new_position = np.array([x, y, z])
+            pendulum_mass.move_to(new_position)
+            rod.put_start_and_end_on(pendulum_origin, pendulum_mass.get_center())
+
+        self.play(UpdateFromAlphaFunc(pendulum_mass, update_pendul  um, run_time=10))
+
+if __name__ == "__main__":
+    scene = SphericalPendulum()
+    scene.render()
